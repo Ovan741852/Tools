5241ccb03c6211ee3817704fbc0c5ddc04a97732
Ovan741852 <77163109+Ovan741852@users.noreply.github.com>
2021-01-08 23:53:13 +0800

    Create PrefabTracker.cs

---
+++ PrefabTracker.cs
@@ -0,0 +1,172 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+public class PrefabTracker
+{
+    [MenuItem("Assets/CustomTools/Prefab/PrefabTracker")]
+    public static void Track()
+    {
+        EditorWindow.GetWindowWithRect<PrefabTrackerWindow>(new Rect(0, 0, 350, 500));
+    }
+
+    [MenuItem("Assets/CustomTools/Prefab/PrefabTracker", true)]
+    private static bool TargetCheck()
+    {
+        var target = Selection.activeObject;
+        var targetType = target.GetType();
+        if (!targetType.Equals(typeof(GameObject)))
+            return false;
+        var assetPath = AssetDatabase.GetAssetPath(target);
+        if (string.IsNullOrEmpty(assetPath))
+            return false;
+        return true;
+    }
+
+    public static Dictionary<string, Queue<string>> FindTypeFiles(Type[] types)
+    {
+        var typeMapping = new Dictionary<string, Queue<string>>();
+        foreach (var type in types)
+        {
+            var key = type.Name;
+            var files = AssetDatabase.FindAssets($"t:{type.Name}").
+                    Select(g => AssetDatabase.GUIDToAssetPath(g));
+            var value = new Queue<string>();
+            foreach (var file in files)
+                value.Enqueue(file);
+            typeMapping[key] = value;
+        }
+
+        return typeMapping;
+    }
+}
+
+public class PrefabTrackerWindow : EditorWindow
+{
+    public static readonly Type[] SearchTypes = new Type[] {
+        typeof(GameObject),
+        typeof(SceneAsset),
+        typeof(ScriptableObject),
+    };
+
+    public static readonly string[] TypeNames =
+        SearchTypes.Select(t => t.Name).ToArray();
+
+    private int _sourceCount;
+    private Dictionary<string, Queue<string>> _sourceMapping;
+
+    private string _targetGuid;
+    private Object _target;
+    private Dictionary<string, List<Object>> _resultMapping = 
+        new Dictionary<string, List<Object>>();
+
+    private Vector2 _position;
+    private Dictionary<string, bool> _foldOutStates = 
+        new Dictionary<string, bool>();
+
+    private void OnEnable()
+    {
+        _target = Selection.activeObject;
+        var assetPath = AssetDatabase.GetAssetPath(_target);
+        _targetGuid = AssetDatabase.AssetPathToGUID(assetPath);
+
+        _foldOutStates.Clear();
+        foreach (var type in SearchTypes)
+            _foldOutStates[type.Name] = false;
+
+        _sourceMapping = PrefabTracker.FindTypeFiles(SearchTypes);
+        _sourceCount = _sourceMapping.Sum(q => q.Value.Count);
+    }
+
+    private void OnGUI()
+    {
+        if (_sourceMapping == null)
+            return;
+        Filt();
+
+        GUI.enabled = false;
+        EditorGUILayout.ObjectField(_target.name, _target, typeof(GameObject), false);
+        GUI.enabled = true;
+
+        _position = EditorGUILayout.BeginScrollView(_position);
+        foreach(var type in SearchTypes)
+            DrawTypeResult(type);
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void Filt()
+    {
+        if (_sourceMapping.Count != 0)
+        {
+            _resultMapping.Clear();
+            int count = 0;
+            EditorUtility.DisplayProgressBar("Searching..", $"0/{_sourceCount}", 0);
+            try
+            {
+                while (count < _sourceCount)
+                {
+                    foreach (var pair in _sourceMapping)
+                    {
+                        var mapping = pair.Value;
+                        var list = new List<Object>();
+                        _resultMapping[pair.Key] = list;
+                        while (mapping.Count != 0)
+                        {
+                            string line;
+                            bool foundGuid = false;
+                            var file = mapping.Dequeue();
+                            using (var reader = new StreamReader(file))
+                            {
+                                while ((line = reader.ReadLine()) != null)
+                                {
+                                    if (!line.Contains(_targetGuid))
+                                        continue;
+                                    foundGuid = true;
+                                    break;
+                                }
+                            }
+
+                            if (foundGuid)
+                                list.Add(AssetDatabase.LoadAssetAtPath<Object>(file));
+                            count++;
+                            EditorUtility.DisplayProgressBar("Searching..", $"{count}/{_sourceCount}", (float)count / _sourceCount);
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            }
+            catch 
+            {
+
+            }
+            EditorUtility.ClearProgressBar();
+
+            _sourceMapping.Clear();
+        }
+    }
+
+    private void DrawTypeResult(Type type)
+    {
+        var typeName = type.Name;
+        var foldOut = _foldOutStates[typeName];
+        var mapping = _resultMapping[typeName];
+        foldOut = EditorGUILayout.BeginFoldoutHeaderGroup(foldOut, $"{typeName}({mapping.Count})");
+        if (foldOut)
+        {
+            GUI.enabled = false;
+            var identLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = identLevel + 1;
+            foreach (var obj in mapping)
+                EditorGUILayout.ObjectField(obj.name, obj, type, false);
+            EditorGUI.indentLevel = identLevel;
+            GUI.enabled = true;
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        _foldOutStates[typeName] = foldOut;
+    }
+}
